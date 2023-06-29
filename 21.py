5a3dd4cc31266096303a17dac30a032caeeba29d
selim <thenvsdcethjpe@jnb.com>
2023-06-29 11:48:08 +0200

    changes

--- 21.py
+++ 21.py
@@ -1,3 +1,7 @@
+# Short Question Description: Evaluate the sum of all the amicable numbers under 10000.
+# .
+# Author: @SelimGunal
+# Finished on 29.06.2023
 
 numsList = {}
 for i in range(1, 10000):
@@ -9,7 +13,15 @@ for i in range(1, 10000):
 
     numsList[i] = sum
 
-# print(numsList[20])
+
+# print(numsList[220])
 
 # make a nested for loop for to contrast the numbers
 
+sum = 0
+for i in range(1, 10000):
+    for x in range(i+1, 10000):
+        if((numsList[i] == x) and (numsList[x] == i)):
+            sum = sum + i + x
+
+print(sum)
