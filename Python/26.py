ab9ac1ef19198e5e51dad96552f08267b0bba883
selim <thenvsdcethjpe@jnb.com>
2023-06-30 21:12:10 +0200

    changes

--- Python/26.py
+++ Python/26.py
@@ -2,41 +2,11 @@
 # Author: @SelimGunal
 # tried on 30.06.2023
 
-longestLongest = 0
-for x in range(1, 1001):
+devir = "123123123"
 
-    theNumber = list(str(1 / x))
+for i in range(0, len(devir)):
 
-    theNumber.pop(0)
-    theNumber.pop(0)
-    user = "".join(theNumber)
-
-    isFinished = False
-    longest = 0
-    longestText = ""
-    for i in range(0, len(user)):
-        text = user[i]
-        for x in range(i + 1, len(user)):
-            if not (user[x] in text):
-                text = text + user[x]
-            else:
-                if len(text) > longest:
-                    longest = len(text)
-                    longestText = text
-                break
-            # When this for loop finishes it continues with outer for loop that's why we should make a isFinished
-            if x == len(user) - 1:
-                isFinished = True
-
-        if isFinished:
-            break
+    for x in range(i, len(devir)):
 
 
-    if longest > longestLongest:
-        longestLongest = longest
-        longestLongestNumber = x
-        longestLongestText = longestText
-
-
-print(longestLongestNumber)
-print(longestLongestText)
+print(currentCycle)
