d8b88f000a51860e34e232d466f5a5f96cedaa26
selim <thenvsdcethjpe@jnb.com>
2023-06-30 20:53:23 +0200

    changes

--- Python/26.py
+++ Python/26.py
@@ -1,29 +1,42 @@
-theNumber = list(str(1/7))
+# Short Question Description: Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+# Author: @SelimGunal
+# tried on 30.06.2023
 
-theNumber.pop(0)
-theNumber.pop(0)
-user = "".join(theNumber)
+longestLongest = 0
+for x in range(1, 1001):
 
-isFinished = False
+    theNumber = list(str(1 / x))
 
-longest = 0
-for i in range(0, len(user)):
-    text = user[i]
-    for x in range(i + 1, len(user)):
-        if not (user[x] in text):
-            text = text + user[x]
-        else:
-            if len(text) > longest:
-                longest = len(text)
-                longestText = text
+    theNumber.pop(0)
+    theNumber.pop(0)
+    user = "".join(theNumber)
+
+    isFinished = False
+    longest = 0
+    longestText = ""
+    for i in range(0, len(user)):
+        text = user[i]
+        for x in range(i + 1, len(user)):
+            if not (user[x] in text):
+                text = text + user[x]
+            else:
+                if len(text) > longest:
+                    longest = len(text)
+                    longestText = text
+                break
+            # When this for loop finishes it continues with outer for loop that's why we should make a isFinished
+            if x == len(user) - 1:
+                isFinished = True
+
+        if isFinished:
             break
-        # When this for loop finishes it continues with outer for loop that's why we should make a isFinished
-        if x == len(user) - 1:
-            isFinished = True
 
-    if isFinished:
-        break
 
-print(longest)
-print(longestText)
+    if longest > longestLongest:
+        longestLongest = longest
+        longestLongestNumber = x
+        longestLongestText = longestText
 
+
+print(longestLongestNumber)
+print(longestLongestText)
