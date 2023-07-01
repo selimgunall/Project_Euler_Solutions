91c2b6bfa398b478bfd5883a5515a7465137dc9d
selim <thenvsdcethjpe@jnb.com>
2023-07-01 15:52:11 +0200

    changes

--- Python/1.py
+++ Python/1.py
@@ -9,11 +9,7 @@ sum = 0
 
 #I've done a for loop because the the question wants the numbers below the one thousand
 for i in range(0 ,1000):
-    if (i % 3 == 0):
+    if (i % 3 == 0 or i % 5 == 0):
         sum = sum + i
-    if (i % 5 == 0):
-        sum = sum + i
-        if (i % 3 == 0):
-            sum = sum - i
 print(sum)
 #The result is 233168
---
+++ test/12.py
@@ -0,0 +1,5 @@
+theNumber = 12
+
+for i in range(1, int(theNumber / 2) + 1):
+    if theNumber % 2 == 0:
+        print(i)
