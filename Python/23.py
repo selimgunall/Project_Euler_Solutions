370bcf04f2a36e6b07d917443251192eb9ce2b5f
selim <thenvsdcethjpe@jnb.com>
2023-07-04 12:57:46 +0200

    finished

--- Python/23.py
+++ Python/23.py
@@ -1,10 +1,7 @@
 
-
 perfect = []
 abundant = []
 for x in range(1, 28123):
-
-
     theNumber = x
     sum = 0
     for i in range(1, theNumber):
@@ -18,20 +15,47 @@ for x in range(1, 28123):
         else:
             abundant.append(theNumber)
 
-print(perfect)
-print(abundant)
+sumNumbers = {}
+for i in range(1, 28123):
+    sumNumbers[i] = i
+    sumNumbers
 
-sumOfTwo = []
-for i in range(0, len(abundant)):
-    for x in range(i, len(abundant)):
-        sumOfTwo.append(i+x)
+print("basladi")
 
-print("gecti")
-sumOfTwo.sort()
+# sum 28122 den kucuk oldugu surece while dongusu yap
+# for i in range(0, len(abundant)):
+#     if i + i > 28123:
+#         break
+#     for x in range(i, len(abundant)):
+#         sum = abundant[i] + abundant[x]
+#         sumNumbers[sum-1] = 0
 
-result = 0
-for i in range(0, 28123):
-    if not(i in sumOfTwo):
-        result += i
+i = 0
+x = i
+sum = 0
+while i < len(abundant):
+    x = i
+    sum = 0
+    while sum < 28123:
+        sum = abundant[i] + abundant[x]
+        sumNumbers[sum] = 0
+        x += 1
 
-print(result)
+    print(sumNumbers[i + 1])
+
+
+    i += 1
+
+
+
+for i in range(1, 28123):
+    print(sumNumbers[i])
+
+print("bitti")
+
+
+sum = 0
+for i in range(1, 28123):
+    sum = sum + sumNumbers[i]
+
+print(sum)
