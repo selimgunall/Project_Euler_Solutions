1b173a8a23ad64fa71db4a83d8c918d9e1b21887
wdaefsgtf <egtreg@drgh.com>
2023-07-17 00:45:33 +0200

    changes

--- Python/35.py
+++ Python/35.py
@@ -1,1 +1,15 @@
+import math
 
+for x in range(0, 1000000):
+
+    i = 1
+
+    isPrime = True
+
+    while isPrime == True and i < math.sqrt(x):
+        if x % i == 0:
+            isPrime = False
+        i += 1
+
+    if isPrime == True:
+        print(x)
