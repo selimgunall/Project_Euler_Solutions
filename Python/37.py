d0dc82a8245cb6b0834fed7037fb1b933fc574c1
wdaefsgtf <egtreg@drgh.com>
2023-07-23 11:34:18 +0200

    changes

--- Python/37.py
+++ Python/37.py
@@ -1,23 +1,21 @@
 
 import math
 
-truncatable = 0
 while truncatable <= 11:
     pass
 
 
 whatNumber = 2
-primeNumbersList = []
-indicator = 0
-
-while whatNumber < 2000000:
-    for x in range (2, int(math.sqrt(whatNumber))+1):
+truncatable = 0
+while truncatable <= 11:
+    for x in range(2, int(math.sqrt(whatNumber))+1):
         indicator = 0
         if whatNumber % x == 0:
             indicator = 1
             break
     if indicator == 0:
-        primeNumbersList.append(whatNumber)
+        pass
+        # my code
     whatNumber = whatNumber + 1
 
 
