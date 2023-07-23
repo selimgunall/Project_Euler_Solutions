76ad298b4bacb79250edc9f00e1a6d07bab4fb17
wdaefsgtf <egtreg@drgh.com>
2023-07-23 13:04:13 +0200

    changes

--- Python/37.py
+++ Python/37.py
@@ -1,21 +1,29 @@
 
 import math
 
-while truncatable <= 11:
-    pass
+def isPrime(whatNumber):
+    for x in range(2, int(math.sqrt(whatNumber))+1):
+        indicator = 0
+        if whatNumber % x == 0:
+            return 1
+    return 0
 
 
 whatNumber = 2
 truncatable = 0
 while truncatable <= 11:
-    for x in range(2, int(math.sqrt(whatNumber))+1):
-        indicator = 0
-        if whatNumber % x == 0:
-            indicator = 1
-            break
-    if indicator == 0:
-        pass
-        # my code
+
+    if isPrime(whatNumber) == 0:
+        strCurrentN = str(whatNumber)
+
+        truncatableListR = []
+        for z in range(len(strCurrentN)-1, -1, -1):
+            truncatableListR.insert(0, strCurrentN[z])
+
+
+
+        truncatableList = []
+        for a in range(0, len(strCurrentN)):
+            truncatableList.append(strCurrentN[a])
+
     whatNumber = whatNumber + 1
-
-
