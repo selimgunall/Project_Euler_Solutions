2b454d9de3f013d144dab2af0be463b1a18366a9
wdaefsgtf <egtreg@drgh.com>
2023-07-23 22:52:00 +0200

    changes

---
+++ test/35.py
@@ -0,0 +1,16 @@
+
+number = 32250
+
+ones = number % 10
+print(ones)
+
+tens = (number % 100) // 10
+print(tens)
+
+hundreds = (number % 1000) // 100
+print(hundreds)
+
+thousand = number // 1
+print(number)
+
+# tenThousand =
