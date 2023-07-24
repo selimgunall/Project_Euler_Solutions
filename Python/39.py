778c7dca43a0e6d0c07a6eadb3281a37dc6cd1fc
wdaefsgtf <egtreg@drgh.com>
2023-07-24 20:41:21 +0200

    changes

--- Python/39.py
+++ Python/39.py
@@ -2,6 +2,11 @@
 # perimeter is "cevre" in turkish
 # temel bir while loop olacak p 1000den kucuk oldugu surece calisacak (a**2 + b**2  =  c**2) (a+b+c = p)
 # while loopun icinde iki tane for loop olacak birise a digeri b icin bunlarin karlerinin toplami kalan sayini karesine esit olcak
+# b loopu a-120 ye kadar gidecek
+
+
+# For which value of p <= 1000, is the number of solutions maximised?
+
 
 p = 1
 
