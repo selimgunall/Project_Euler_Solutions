b1ad5d519c75b13fdbdc6785f5f8403aca9e56e5
wdaefsgtf <egtreg@drgh.com>
2023-07-24 20:29:47 +0200

    changes

--- Python/39.py
+++ Python/39.py
@@ -1,4 +1,13 @@
 # right angled triangle is "dik ucgen" in turkish
 # perimeter is "cevre" in turkish
-# temel bir while loop olacak p 1000den kucuk oldugu surece calisacak
-#
+# temel bir while loop olacak p 1000den kucuk oldugu surece calisacak (a**2 + b**2  =  c**2) (a+b+c = p)
+# while loopun icinde iki tane for loop olacak birise a digeri b icin bunlarin karlerinin toplami kalan sayini karesine esit olcak
+
+p = 1
+
+while p <= 1000:
+
+
+
+    p += 1
+
