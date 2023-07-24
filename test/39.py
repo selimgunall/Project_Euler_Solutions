ee03d3244e9ef4f403d2b4eb5c6b794f3ca3e5c4
wdaefsgtf <egtreg@drgh.com>
2023-07-24 21:09:33 +0200

    finsihed

--- test/39.py
+++ test/39.py
@@ -1,12 +1,11 @@
 import math
-p = 1
-
 
 p = 120
 # a length side
 for a in range(1, p):
     # b length side this goes until p-a+1 because for example p=1000 and a = 998 so b must maximum be 2
-    for b in range(1, p - a + 1):
+    # we are starting from a becuse while we are counting for the sum it reverses the number such as 20 40  to  40 20
+    for b in range(a, p - a + 1):
 
         sqrtAandB = math.sqrt(a**2 + b**2)
 
@@ -16,4 +15,3 @@ for a in range(1, p):
         # in this if loop we are going to try if sqrt of (p - (a + b)) equals c
         if sqrtAandB == pMinusAandB:
             print(a, b, pMinusAandB)
-
