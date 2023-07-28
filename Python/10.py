d51bb5cadf23a410e0e754b42d9d71fb89535415
wdaefsgtf <egtreg@drgh.com>
2023-07-28 20:40:53 +0200

    finished

--- Python/10.py
+++ Python/10.py
@@ -1,18 +1,24 @@
-# Short Question Description: !!!
+# Short Question Description: Find the sum of all the primes below two million.
 # Author: @SelimGunal
-# Finished on !!!
-whatNumber = 2
-primeNumbersList = []
-indicator = 0
+# Finished on 28.07.2023
 
-while (whatNumber < 2000000):
-    for x in range (2 ,whatNumber):
-        indicator = 0
-        if (whatNumber % x == 0):
-            indicator = 1
-            break
-    if (indicator == 0):
-        primeNumbersList.append(whatNumber)
-    whatNumber = whatNumber + 1
-print(primeNumbersList)
-#The result is
+import math
+
+# this function checks if provided number is prime
+def isPrime(whatNumber):
+    x = 2
+    while x <= int(math.sqrt(whatNumber)) + 1:
+        if (whatNumber % x == 0 and whatNumber != 2) or whatNumber == 1:
+            return 0
+        x += 1
+    return 1
+
+i = 1
+result = 0
+while i < 2000000:
+    if isPrime(i):
+        result += i
+    i += 1
+
+#The result is142913828922 in 45 seconds
+print(result)
